dd24ac8f05edded0eb5a7ffca8cdf47540773500
임형섭 <imhyeongseob@imhyeongseob-ui-MacBookPro.local>
2025-04-24 13:42:06 +0900

    과제 제출

---
+++ Practice/20250423/기본/ch12/sec03/exam01/Member.java
@@ -0,0 +1,17 @@
+
+package ch12.sec03.exam01;
+public class Member {
+    public String id;
+    public Member(String id) {
+        this.id = id;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Member target) { // 타입 감사
+            if(id.equals(target.id)) { // id 문자열이 같은지 비교
+                return true;
+            }
+        }
+        return false;
+    }
+}
